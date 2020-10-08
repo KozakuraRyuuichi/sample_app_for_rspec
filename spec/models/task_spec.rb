7b9ec4c2cdf7ec27e2ba7ebcf2c61510d86e92f6
KozakuraRyuuichi <k267280.352@gmail.com>
2020-10-08 22:34:37 +0900

    Fix: 役割が分かるような変数名に変更

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -9,32 +9,32 @@ RSpec.describe Task, type: :model do
 
     # タイトルが存在しない場合は無効であること
     it "is invalid without title" do
-      task = FactoryBot.build(:task, title: nil)
-      task.valid?
-      expect(task.errors[:title]).to include("can't be blank")
+      task_without_title = FactoryBot.build(:task, title: nil)
+      task_without_title.valid?
+      expect(task_without_title.errors[:title]).to include("can't be blank")
     end
 
     # タイトルが重複する場合は無効であること
     it "is invalid with a duplicate title" do
       FactoryBot.create(:task, title: "title1")
-      task = FactoryBot.build(:task, title: "title1")
-      task.valid?
-      expect(task.errors[:title]).to include("has already been taken")
+      task_with_duplicated_title = FactoryBot.build(:task, title: "title1")
+      task_with_duplicated_title.valid?
+      expect(task_with_duplicated_title.errors[:title]).to include("has already been taken")
     end
 
     # ステータスが存在しない場合は無効であること
     it "is invalid without status" do
-      task = FactoryBot.build(:task, status: nil)
-      task.valid?
-      expect(task.errors[:status]).to include("can't be blank")
+      task_without_status = FactoryBot.build(:task, status: nil)
+      task_without_status.valid?
+      expect(task_without_statusk.errors[:status]).to include("can't be blank")
     end
 
     # 別のタイトルで有効であること
     it "is valid with another title" do
       FactoryBot.create(:task)
-      task = FactoryBot.build(:task, title: "title2")
-      task.valid?
-      expect(task).to be_valid
+      task_with_another_title = FactoryBot.build(:task, title: "title2")
+      task_with_another_title.valid?
+      expect(task_with_another_title).to be_valid
     end
   end
 end
