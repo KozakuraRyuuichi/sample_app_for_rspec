71d1812d48284990c4e7aba249e4a01f73066058
KozakuraRyuuichi <k267280.352@gmail.com>
2020-10-08 22:43:40 +0900

    Fix: FactoryBotを省略した

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,35 +4,36 @@ RSpec.describe Task, type: :model do
   describe 'validation' do
     # すべての属性で有効であること
     it "is valid with all attributes" do
-      expect(FactoryBot.build(:task)).to be_valid
+      task = build(:task)
+      expect(task).to be_valid
     end
 
     # タイトルが存在しない場合は無効であること
     it "is invalid without title" do
-      task_without_title = FactoryBot.build(:task, title: nil)
+      task_without_title = build(:task, title: nil)
       task_without_title.valid?
       expect(task_without_title.errors[:title]).to include("can't be blank")
     end
 
     # タイトルが重複する場合は無効であること
     it "is invalid with a duplicate title" do
-      FactoryBot.create(:task, title: "title1")
-      task_with_duplicated_title = FactoryBot.build(:task, title: "title1")
+      task = create(:task)
+      task_with_duplicated_title = build(:task, title: task.title)
       task_with_duplicated_title.valid?
       expect(task_with_duplicated_title.errors[:title]).to include("has already been taken")
     end
 
     # ステータスが存在しない場合は無効であること
     it "is invalid without status" do
-      task_without_status = FactoryBot.build(:task, status: nil)
+      task_without_status = build(:task, status: nil)
       task_without_status.valid?
       expect(task_without_statusk.errors[:status]).to include("can't be blank")
     end
 
     # 別のタイトルで有効であること
     it "is valid with another title" do
-      FactoryBot.create(:task)
-      task_with_another_title = FactoryBot.build(:task, title: "title2")
+      task = create(:task)
+      task_with_another_title = build(:task, title: "title2")
       task_with_another_title.valid?
       expect(task_with_another_title).to be_valid
     end
