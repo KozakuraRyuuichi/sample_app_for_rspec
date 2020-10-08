65590ac350d07d7809258bcce8f51c3ff6bcd06d
KozakuraRyuuichi <k267280.352@gmail.com>
2020-10-08 22:53:40 +0900

    Fix: rspec-rails gemが提供するマッチャでの記述に変更

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -6,12 +6,13 @@ RSpec.describe Task, type: :model do
     it "is valid with all attributes" do
       task = build(:task)
       expect(task).to be_valid
+      expect(task.errors).to be_empty
     end
 
     # タイトルが存在しない場合は無効であること
     it "is invalid without title" do
       task_without_title = build(:task, title: nil)
-      task_without_title.valid?
+      expect(task_without_title).to be_invalid
       expect(task_without_title.errors[:title]).to include("can't be blank")
     end
 
@@ -19,23 +20,23 @@ RSpec.describe Task, type: :model do
     it "is invalid with a duplicate title" do
       task = create(:task)
       task_with_duplicated_title = build(:task, title: task.title)
-      task_with_duplicated_title.valid?
+      expect(task_with_duplicated_title).to be_invalid
       expect(task_with_duplicated_title.errors[:title]).to include("has already been taken")
     end
 
     # ステータスが存在しない場合は無効であること
     it "is invalid without status" do
       task_without_status = build(:task, status: nil)
-      task_without_status.valid?
-      expect(task_without_statusk.errors[:status]).to include("can't be blank")
+      expect(task_without_status).to be_invalid
+      expect(task_without_status.errors[:status]).to include("can't be blank")
     end
 
     # 別のタイトルで有効であること
     it "is valid with another title" do
       task = create(:task)
       task_with_another_title = build(:task, title: "title2")
-      task_with_another_title.valid?
       expect(task_with_another_title).to be_valid
+      expect(task_with_another_title.errors).to be_empty
     end
   end
 end
