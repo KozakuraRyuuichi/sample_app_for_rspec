84c91b7e930dbc28facf1851e00d2be8801c99d4
KozakuraRyuuichi <k267280.352@gmail.com>
2020-10-09 20:59:48 +0900

    Fix: 波括弧内に半角スペースを設けて見やすくした

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     # sequence(:email) { |n| "user#{n}@gmail.com"}からの書き換え
     sequence(:email, "user1@example.com")
-    password {"password"}
-    password_confirmation {"password"}
+    password { "password" }
+    password_confirmation { "password" }
   end
 end
