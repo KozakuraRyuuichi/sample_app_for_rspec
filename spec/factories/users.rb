e3556e838a6e9d1037058fde18f1aac79e6c301f
KozakuraRyuuichi <56499977+KozakuraRyuuichi@users.noreply.github.com>
2020-10-09 12:28:07 +0900

    Update spec/factories/users.rb
    Co-authored-by: YUJI ITO <yuji91.1227@gmail.com>

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     # sequence(:email) { |n| "user#{n}@gmail.com"}からの書き換え
-    sequence(:email, "user1@sample.com")
+    sequence(:email, "user1@example.com")
     password {"password"}
     password_confirmation {"password"}
   end
