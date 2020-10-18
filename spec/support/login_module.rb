f42baeeaf339a2ac6cf6c7a9f7b2da5112932a83
KozakuraRyuuichi <k267280.352@gmail.com>
2020-10-18 18:37:08 +0900

    [Fix]typoの修正

--- spec/support/login_module.rb
+++ spec/support/login_module.rb
@@ -1,7 +1,7 @@
 module LoginModule
   def login(user)
     visit login_path
-    fill_in 'Email', with: user.Email
+    fill_in 'Email', with: user.email
     fill_in 'Password', with: 'password'
     click_button 'Login'
   end
