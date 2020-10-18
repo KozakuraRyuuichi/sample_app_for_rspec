a462da4fd78ab84b4e6fdb702cff0def5152761b
KozakuraRyuuichi <k267280.352@gmail.com>
2020-10-18 18:42:04 +0900

    [update]LoginModuleの反映

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -53,20 +53,17 @@ RSpec.describe 'Users', type: :system do
   end
 
   describe 'ログイン後' do
+    before { login(user) }
     describe 'ユーザー編集' do
       context 'フォームの入力値が正常' do
         it 'ユーザーの編集が成功する' do
-          visit login_path
-          fill_in 'Email', with: user.email
-          fill_in 'Password', with: 'password'
-          click_button 'Login'
           expect(current_path).to eq root_path
           expect(page).to have_content("Login successful")
           click_on 'Mypage'
           click_on 'Edit'
           fill_in 'Email', with: user.email
-          fill_in 'Password', with: 'new_password'
-          fill_in 'Password confirmation', with: 'new_password'
+          fill_in 'Password', with: 'update_password'
+          fill_in 'Password confirmation', with: 'update_password'
           click_button 'Update'
           expect(current_path).to eq user_path(user.id)
           expect(page).to have_content("User was successfully updated.")
@@ -74,10 +71,6 @@ RSpec.describe 'Users', type: :system do
       end
       context 'メールアドレスが未入力' do
         it 'ユーザーの編集が失敗する' do
-          visit login_path
-          fill_in 'Email', with: user.email
-          fill_in 'Password', with: 'password'
-          click_button 'Login'
           click_on 'Mypage'
           click_on 'Edit'
           fill_in 'Email', with: ''
@@ -94,10 +87,6 @@ RSpec.describe 'Users', type: :system do
                                 email: 'email@example.com',
                                 password: 'another_password',
                                 password_confirmation: 'another_password')
-          visit login_path
-          fill_in 'Email', with: user.email
-          fill_in 'Password', with: 'password'
-          click_button 'Login'
           click_on 'Mypage'
           click_on 'Edit'
           fill_in 'Email', with: another_user.email
@@ -114,10 +103,6 @@ RSpec.describe 'Users', type: :system do
                                 email: 'email@example.com',
                                 password: 'another_password',
                                 password_confirmation: 'another_password')
-          visit login_path
-          fill_in 'Email', with: user.email
-          fill_in 'Password', with: 'password'
-          click_button 'Login'
           click_on 'Mypage'
           visit edit_user_path(another_user.id)
           expect(page).to have_content("Forbidden access.")
@@ -133,10 +118,6 @@ RSpec.describe 'Users', type: :system do
       context 'タスクを作成' do
         it '新規作成したタスクが表示される' do
           visit user_path(user)
-          visit login_path
-          fill_in 'Email', with: user.email
-          fill_in 'Password', with: 'password'
-          click_button 'Login'
           click_on 'New task'
           create(:task, title: 'test_title', status: :todo, user: user)
           visit user_path(user)
