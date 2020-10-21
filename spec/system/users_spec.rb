19eb8f21f4727fa2dae437db97ee48a08b8492e8
KozakuraRyuuichi <k267280.352@gmail.com>
2020-10-21 09:16:35 +0900

    Fix 登録済みのメールアドレスを使用した場合の、記述を修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -31,14 +31,16 @@ RSpec.describe 'Users', type: :system do
       end
       context '登録済みのメールアドレスを使用' do
         it 'ユーザーの新規登録に失敗する' do
+          existed_user = create(:user)
           visit new_user_path
-          fill_in 'Email', with: user.email
+          fill_in 'Email', with: existed_user.email
           fill_in 'Password', with: 'password'
           fill_in 'Password confirmation', with: 'password'
           click_button 'SignUp'
           expect(page).to have_content '1 error prohibited this user from being saved'
           expect(page).to have_content("Email has already been taken")
           expect(current_path).to eq users_path
+          expect(page).to have_field 'Email', with: existed_user.email
         end
       end
     end
